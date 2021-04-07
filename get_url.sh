431f568d4874dad9ce095edbbaa73ba0f6cd3f63
Jason Leitch <jason.leitch@arnoldclark.com>
2021-04-07 11:51:46 +0100

    adding temp url for awx

--- get_url.sh
+++ get_url.sh
@@ -1,2 +1,3 @@
 nodePort=$(kubectl get service -n ingress-nginx -o json | jq '.items[].spec.ports[]|select(.name == "https")|.nodePort'|grep -v null)
 echo https://kubernetes.docker.internal:${nodePort}/healthz
+echo https://awx.example.com:${nodePort}/
