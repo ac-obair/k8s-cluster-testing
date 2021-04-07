df8ebbc0f000aa0e7e903b9101d9ac999dcea69b
Jason Leitch <jason.leitch@arnoldclark.com>
2021-04-07 12:49:26 +0100

    adding token

--- awxkit_env.sh
+++ awxkit_env.sh
@@ -3,3 +3,5 @@ PASSWORD=$(kubectl get secrets -n default awx-admin-password -o template --templ
 export TOWER_HOST=https://awx.example.com:32208
 export TOWER_USERNAME=admin 
 export TOWER_PASSWORD=${PASSWORD}
+TOKEN=$(awx login -k | jq '.token' | tr -d '"')
+export TOWER_TOKEN=${TOKEN}
