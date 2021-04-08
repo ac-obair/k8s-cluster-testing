f4520a7d34f1490bc49f9a2127b8c3428d6157e1
Jason Leitch <jason.leitch@arnoldclark.com>
2021-04-08 10:12:52 +0100

    removing comment and updating db user

--- awx/scripts/test-external-postgre-azure.sh
+++ awx/scripts/test-external-postgre-azure.sh
@@ -3,11 +3,9 @@
 # az cli build db
 PASSWORD=LetjIma1o3uQYbs2cq1GJYkI!
 HOST=awx-external-postgres.postgres.database.azure.com
-USER=awx@awx-external-postgres
+USER=psqladminun@awx-external-postgres
 DBNAME=postgres
 #DBNAME=awx
-
-TF rather than arm? do arm just to do it
 
 kubectl exec -it awx-postgres-0 -n default -- /bin/bash -c "PGPASSWORD=${PASSWORD} psql -h ${HOST} -U ${USER} -d ${DBNAME} -c '\conninfo'"
 kubectl exec -it awx-postgres-0 -n default -- /bin/bash -c "PGPASSWORD=${PASSWORD} psql -h ${HOST} -U ${USER} -d ${DBNAME} --list"
