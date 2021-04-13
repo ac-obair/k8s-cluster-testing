1ea006f0c681fa3e028c163bf9d4d2f24d1db153
Jason Leitch <jason.leitch@arnoldclark.com>
2021-04-13 13:18:40 +0100

    adding awxkit env

--- awx/scripts/set-awxkit-env.sh
+++ awx/scripts/set-awxkit-env.sh
@@ -1,6 +1,6 @@
 # for testing
 PASSWORD=$(kubectl get secrets -n default awx-admin-password -o template --template={{.data.password}} | base64 -d)
-export TOWER_HOST=https://awx.example.com:32208
+export TOWER_HOST=https://awx.example.com:30789
 export TOWER_USERNAME=admin 
 export TOWER_PASSWORD=${PASSWORD}
 TOKEN=$(awx login -k | jq '.token' | tr -d '"')
