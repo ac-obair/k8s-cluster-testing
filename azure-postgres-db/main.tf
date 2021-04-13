f2984b0a1b11ae0cd8b7d63c4704afa82cf41a4b
Jason Leitch <jason.leitch@arnoldclark.com>
2021-04-13 13:53:36 +0100

    removing duplicate provider block

--- azure-postgres-db/main.tf
+++ azure-postgres-db/main.tf
@@ -4,12 +4,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  version         = "=2.16.0"
-  subscription_id = "0c0e5228-4139-4cc4-bfc0-8601fb17134a"
-  features {}
-}
-
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "awx-test-keyvault" {
